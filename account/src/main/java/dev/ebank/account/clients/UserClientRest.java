3e8ee8c790ec17aff991201bb9a840c4e2d0ae95
latifatou Iyali <97948683+latifahamraou@users.noreply.github.com>
2024-02-02 23:46:47 +0100

    Update UserClientRest.java

--- account/src/main/java/dev/ebank/account/clients/UserClientRest.java
+++ account/src/main/java/dev/ebank/account/clients/UserClientRest.java
@@ -1,4 +1,35 @@
 package dev.ebank.account.clients;
 
-public class UserClientRest {
+import dev.ebank.account.model.classes.User;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "USER-SERVICE")
+public interface UserClientRest {
+    @GetMapping("/users")
+    @CircuitBreaker(name = "userService", fallbackMethod = "getAllUsers")
+    List<User> allUsers();
+
+    @GetMapping("/users/{id}")
+    @CircuitBreaker(name = "userService", fallbackMethod = "getUser")
+    User findUserById(@PathVariable Long id);
+
+    default List<User> getAllUsers(Exception e){
+        return List.of();
+    }
+    default User getUser(Long id, Exception e){
+        User user= User.builder()
+                .id(id)
+                .firstname("not vailable")
+                .lastname("not vailable")
+                .email("not vailable")
+                .active(true)
+                .build();
+        return user;
+
+    }
 }
